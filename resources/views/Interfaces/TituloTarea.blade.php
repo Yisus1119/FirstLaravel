b634c07ad2c9c119efbf9b307236dd3a0d4be490
Psycho Thinker <paraquezada@gmail.com>
2021-10-25 23:16:43 -0400

    Se solucionó el problema al insertar registros

--- resources/views/Interfaces/TituloTarea.blade.php
+++ resources/views/Interfaces/TituloTarea.blade.php
@@ -7,9 +7,10 @@
 @csrf <!--Cross-Site Request Forgery, es un metodo para protegernos de los atacantes. -->
 <div class="container w-25 border p-4 mt-4" style="background-color: #283747;">
 <div class="form-floating mb-3">
-  <input type="text" class="form-control" id="floatingInput" placeholder="Debe escribir el titulo de la tarea.">
+  <input type="text" class="form-control" id="floatingInput" placeholder="Debe escribir el titulo de la tarea." name="Titulo">
   <label for="floatingInput">T&iacute;tulo de la tarea</label>
 </div>
 <button type="submit" class="btn btn-primary">Crear nueva tarea</button>
 </form>
+</div>
 @endsection
